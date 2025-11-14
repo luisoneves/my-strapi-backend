016a469e82854ab00fd652d6d2ffcbc4e925d8ae
luisoneves <1991lotavion@gmail.com>
2025-11-14 14:41:30 -0300

    feat: Implementar config Nixpacks e separar DB de desenvolvimento

---
+++ config/env/development/database.ts
@@ -0,0 +1,9 @@
+export default {
+  connection: {
+    client: 'sqlite',
+    connection: {
+      filename: '.tmp/data.db',
+    },
+    useNullAsDefault: true,
+  },
+};
---
+++ dist/config/env/development/database.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    connection: {
+        client: 'sqlite',
+        connection: {
+            filename: '.tmp/data.db',
+        },
+        useNullAsDefault: true,
+    },
+};
