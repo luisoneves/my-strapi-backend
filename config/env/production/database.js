8529c4227b91409e0a49725c01b58d395cc98275
luisoneves <1991lotavion@gmail.com>
2025-11-14 06:29:33 -0300

    refactor: Usar configuração de DB explícita para produção

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,16 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: {
-          rejectUnauthorized: false, // Importante para a maioria dos provedores em nuvem
-        },
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+        rejectUnauthorized: false,
       },
-      debug: false,
     },
-  };
-};
+    debug: false,
+  },
+});
