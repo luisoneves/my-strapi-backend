ac4790f46720a1956cb848bb7f5e804f86bba952
luisoneves <1991lotavion@gmail.com>
2025-11-14 06:41:28 -0300

    build: Pré-construir a aplicação para o deploy

---
+++ dist/src/api/configuracao-geral/routes/configuracao-geral.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * configuracao-geral router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::configuracao-geral.configuracao-geral');
---
+++ dist/src/api/servico/controllers/servico.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * servico controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::servico.servico');
